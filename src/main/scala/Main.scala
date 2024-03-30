8172da85f53cc2e306975564cfad67c41b8a242e
nixonggrk <165466911+nixonggrk@users.noreply.github.com>
2024-03-30 16:59:29 +0530

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,40 +3,55 @@ package com.sundogsoftware.spark
 import org.apache.spark.sql._
 import org.apache.log4j._
 
-def techMahindra(spark: SparkSession): DataFrame = {
+def sensorStat(spark: SparkSession): DataFrame = {
  import spark.implicts._ 
  
- // Define the schema for Customers DataFrame
- val customersSchema = StructType(Seq(
- StructField("Id", IntegerType, nullable = true),
- StructField("NameCust", StringType, nullable = true)
+ // Define the schema for Leaderone DataFrame
+ val leaderoneSchema = StructType(Seq(
+ StructField("SensorId", StringType, nullable = true)
+ StructField("Humidity", IntegerType, nullable = true),
+ 
  ))
  
- // Define the data for Customers
- val customersData = Seq(
- Row(1, "Joe"),
- Row(2, "Henry"),
- Row(3, "Sam"),
- Row(4, "Max")
+ // Define the data for Leaderone
+ val leaderoneData = Seq(
+ Row("s1", 10),
+ Row("s2", 88),
+ Row("s1", NaN)
  )
  
- // Define the schema for Orders DataFrame
- val ordersSchema = StructType(Seq(
- StructField("Id", IntegerType, nullable = true),
- StructField("CustomerId", IntegerType, nullable = true)
+ // Define the schema for Leadertwo DataFrame
+ val leadertwoSchema = StructType(Seq(
+ StructField("SensorId", StringType, nullable = true)
+ StructField("Humidity", IntegerType, nullable = true),
+ 
  ))
  
- // Define the data for Orders
- val ordersData = Seq(
- Row(1, 3),
- Row(2, 1)
+ // Define the data for Leadertwo
+ val leadertwoData = Seq(
+ Row(s2, 80),
+ Row(s3, NaN),
+ Row(s2, 78),
+ Row(s1, 98)
  )
- 
- val customersDF = spark.createDataFrame(spark.sparkContext.parallelize(customersData), customersSchema)
+ var i = 0 
+ val leaderoneDF = spark.createDataFrame(spark.sparkContext.parallelize(leaderoneData), leaderoneSchema)
+  i = i + 1
 
- val ordersDF = spark.createDataFrame(spark.sparkContext.parallelize(ordersData), ordersSchema)
+ val leadertwoDF = spark.createDataFrame(spark.sparkContext.parallelize(leadertwoData), leadertwoSchema)
+  i = i + 1 
+ println("Num of processed files:" + i)
  
- val resultDF = customersDF.join(ordersDF, customersDF("Id") === ordersDF("CustomerId"), "leftanti")
+ val resultDF = leaderoneDF.union(leadertwoDF)
+ var j = 0
+ j = resultDF.count()
+ println("Num of processed measurements:" + j)
+ println('Num of failed measurements:)
+ println(resultDF.where(resultDF.Humidity == 'NaN').count()) 
 
- resultDF 
+ windowPartitionAgg = Window.partitionBy("SensorId").orderBy("Humidity")
+ 
+ resultDF.withColumn("Avg",avg(col("Humidity")).over(windowPartitionAgg)).show()
+ resultDF.withColumn("Min",min(col("Humidity")).over(windowPartitionAgg)).show()
+ resultDF.withColumn("Max",max(col("Humidity")).over(windowPartitionAgg)).show()
 }
